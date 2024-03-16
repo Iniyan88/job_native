73baab8832cf48db7d5ef48ce3c47593f1a6dba9
Iniyan88 <biniyan2004@gmail.com>
2024-03-16 13:32:25 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,49 @@
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+export default function Page() {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+            elevation: 0,
+          },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.lightWhite,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
